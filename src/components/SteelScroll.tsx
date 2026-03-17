2a148b8dfd18598c4a37fd8a7b9a3091cc954a12
trendsandtactics <trendsntactics@gmail.com>
2026-03-17 22:39:57 +0530

    Update SteelScroll.tsx

--- src/components/SteelScroll.tsx
+++ src/components/SteelScroll.tsx
@@ -9,7 +9,7 @@ export default function SteelScroll() {
       {/* Background Video */}
       <video
         className="absolute inset-0 w-full h-full object-cover"
-        src="/enhanced-lack2.mp4"
+        src="/hero5.mp4"
         autoPlay
         muted
         loop
